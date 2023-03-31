561dc08744c17cfaddfb3d1ef2c62a5917597225
yeon0517 <rhfmrhfm12324@naver.com>
2023-03-31 13:51:29 +0900

    비밀번호,,,,,까지...

--- WebContent/assets/js/join.js
+++ WebContent/assets/js/join.js
@@ -38,6 +38,29 @@ $idInput.on('blur', function(){
 });
 
 // 여기에 비밀번호 이벤트 넣기.
+const regex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[a-zA-Z\d!@#$%^&*()_+]{8,}$/;
+
+$pwInput.on('blur', function(){
+   if(regex.test(  $(this).val()   )){
+      $checkPwMsg.text("사용 가능한 비밀번호입니다.");
+   }else{
+      $checkPwMsg.html("사용 불가능한 비밀번호입니다. <br>영어, 숫자, 특수문자를 포함하여 8글자 이상 작성하세요!");
+   }
+});
+
+
+/*$('form').on('submit', function(e) {
+	 e.preventDefault(); //기본 이벤트를 막아주는 명령어
+
+	console.log($('#agree').prop('checked'));
+	
+	if($('#agree').prop('checked')){
+		this.submit(); //서브밋 이벤트를 발생시키는 메소드(폼 요소에 사용해야함)
+	} else {
+		alert('약관에 동의해주세요');
+	}
+	
+});*/
 
 
 
