66f04c560db0ea7c31995e9ca010ee26f0408f3f
yeon0517 <rhfmrhfm12324@naver.com>
2023-04-04 10:41:22 +0900

    풀받기전 커밋

--- src/com/cookpang/app/user/FindPasswordOkController.java
+++ src/com/cookpang/app/user/FindPasswordOkController.java
@@ -15,13 +15,26 @@ import com.cookpang.app.user.dto.UserDTO;
 
 
 public class FindPasswordOkController implements Execute {
-	@Override
-	public void execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		UserDAO userDAO = new UserDAO();
-		UserDTO userDTO = new UserDTO();
-		
-		
-		
-		
-	}
+    @Override
+    public void execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserDAO userDAO = new UserDAO();
+        UserDTO userDTO = new UserDTO();
+
+        req.setCharacterEncoding("utf-8");
+
+        userDTO.setUserPhoneNumber(req.getParameter("userPhoneNumber"));
+        userDTO.setUserEmail(req.getParameter("userEmail"));
+
+        String password = userDAO.findPassword(userDTO);
+
+        if (password == null) { // 비밀번호가 없을 경우
+            req.setAttribute("errorMsg", "일치하는 정보가 없습니다.");
+            req.getRequestDispatcher("/user/findPasswordOk.us").forward(req, resp);
+        } else { // 비밀번호가 있을 경우
+            req.setAttribute("password", password);
+            req.setAttribute("user", userDTO); // userDTO를 jsp에서 사용하기 위해 req.setAttribute()를 사용합니다.
+            req.getRequestDispatcher("/user/findPasswordOk.us").forward(req, resp);
+        }
+
+    }
 }
