1a7e3638b3d55942c7f74862360f44a43888cbf4
nsoe12 <nsoe12@naver.com>
2023-04-04 16:13:30 +0900

    제발 오류는 그만

---
+++ src/com/cookpang/app/user/profileEdit/ProfileSelectController.java
@@ -0,0 +1,36 @@
+package com.cookpang.app.user.profileEdit;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.cookpang.app.Execute;
+import com.cookpang.app.user.dao.UserDAO;
+import com.cookpang.app.user.profileEdit.vo.ProfileEditVO;
+
+public class ProfileSelectController implements Execute {
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		UserDAO userDAO = new UserDAO();
+//		UserDTO userDTO = new UserDTO();
+		ProfileEditVO profileVO = new ProfileEditVO();
+//		ProfileEditVO profileEditVO = new ProfileEditVO();
+
+		req.setCharacterEncoding("utf-8");
+		int helpme = 1;
+		
+//		userDTO.setUserId(req.getParameter("userId"));
+//		userDTO.setUserPassword(req.getParameter("userPassword"));
+
+		
+		profileVO = userDAO.getUserProfileInfo(helpme);
+		
+		req.setAttribute("profileVO", profileVO);
+		
+
+		req.getRequestDispatcher("/app/editProfile.jsp").forward(req, resp);
+	
+	}
+}
